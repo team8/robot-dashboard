77f76283e28f483ede6f8939441cac8f315fba43
Robbie S <robbie@selwynsoftware.com>
2016-06-27 19:28:48 -0400

    Created a navbar with a lock.

--- JS/Core.js
+++ JS/Core.js
@@ -1,4 +1,60 @@
+/*
+Document structural elements.
+*/
+var Navbar = {
+	locked: false,
+	img: {
+		elem: null,
+		unlocked_img: "../Images/unlocked.jpeg",
+		lock_img: "../Images/locked_img.jpeg"
+	},
+	MatchView: {
+		in_: true,
+		elem: null
+	},
+	DebugView: {
+		in_: false,
+		elem: null
+	}
+}
+
+function setElementStatus() {
+	Navbar.img.elem = document.getElementById("locked")
+	Navbar.MatchView.elem = document.getElementsByClassName("innerheader")[0]
+	Navbar.DebugView.elem = document.getElementsByClassName("innerheader")[1]
+}
+
+function onNavClick(view) {
+	if (view == 'match') {
+		if (Navbar.locked == true) return
+		Navbar.MatchView.in_ = !Navbar.MatchView.in_ 
+		Navbar.DebugView.in_ = !Navbar.MatchView.in_
+	}
+	else {
+		// view == 'debug'
+		if (Navbar.locked == true) return
+		Navbar.DebugView.in_ = !Navbar.DebugView.in_
+		Navbar.MatchView.in_ = !Navbar.DebugView.in_
+	}
+}
+
+/*
+Updates the Status of the Lock Icon
+*/
+function updateLocked() {
+	Navbar.locked = !Navbar.locked
+	if (Navbar.locked) {
+		Navbar.img.elem.src = Navbar.img.lock_img;
+	}
+	else {
+		Navbar.img.elem.src = Navbar.img.unlocked_img;
+	}
+}
+
+
 $(document).ready(function(){
+	setElementStatus()
+	updateLocked()
 	// sets a function that will be called when the websocket connects/disconnects
 	NetworkTables.addWsConnectionListener(onNetworkTablesConnection, true);
 	// sets a function that will be called when the robot connects/disconnects
@@ -7,7 +63,9 @@ $(document).ready(function(){
 	NetworkTables.addGlobalListener(onValueChanged, true);
 });
 
-
+/*
+NetworkTable functions.
+*/
 function onRobotConnection(connected) {}
 function onNetworkTablesConnection(connected) {}
 function onValueChanged(key, value, isNew) {}
